123f4201eeb850a868f33bc8db2956cfe1dc4b62
송금현 <kumh1999@gmail.com>
2022-09-14 23:15:13 +0900

    DB 연결

---
+++ src/main/java/com/lgcns/mcs/LgMcsApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.mcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgMcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgMcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgcns/mcs/LgMcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.mcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgMcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
